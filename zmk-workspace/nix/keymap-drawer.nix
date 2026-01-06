eb53062948532d043ad853d220467c38e9520a15
mwlaboratories <tb_vdb@hotmail.com>
2026-01-06 20:29:53 +0100

    keymap add

--- zmk-workspace/nix/keymap-drawer.nix
+++ zmk-workspace/nix/keymap-drawer.nix
@@ -3,6 +3,7 @@
 , buildPythonPackage
 , callPackage
 , fetchFromGitHub
+, fetchPypi
 , setuptools
 , poetry-core
 , pydantic
@@ -15,24 +16,35 @@
 }:
 let
   tree-sitter-devicetree = callPackage ./tree-sitter-devicetree.nix {};
+
+  # Pin tree-sitter to 0.24.0 (keymap-drawer requires >=0.24.0,<0.25.0)
+  tree-sitter-pinned = tree-sitter.overridePythonAttrs (old: rec {
+    version = "0.24.0";
+    src = fetchPypi {
+      pname = "tree-sitter";
+      inherit version;
+      hash = "sha256-q9la9lyi9Pfso1Y0M5HtZp52Tzd0i1NSlG8A9/x45zQ=";
+    };
+    doCheck = false;  # Tests not present in this version
+  });
 in
 buildPythonApplication rec {
   pname = "keymap-drawer";
-  version = "0.21.0";
+  version = "0.22.1";
   pyproject = true;
 
   src = fetchFromGitHub {
     owner = "caksoylar";
     repo = pname;
-    rev = "855933863fcc6f0db6098a03e679319dbf7f8bf2";
-    hash = "sha256-InddS9NxVrYOufiP7iWQTQ3VBeJgX2UlBA+Gf7ZfFrI=";
+    rev = "v${version}";
+    hash = "sha256-X3O5yspEdey03YQ6JsYN/DE9NUiq148u1W6LQpUQ3ns=";
   };
 
   build-system = [ poetry-core ];
 
   postPatch = ''
     substituteInPlace pyproject.toml \
-      --replace "[tool.poetry]" "[tool.poetry]
+      --replace-warn "[tool.poetry]" "[tool.poetry]
 name = \"${pname}\"
 version = \"${version}\"
 description = \"${meta.description}\"
@@ -46,7 +58,7 @@ authors = [\"Cem Aksoylar <caksoylar@gmail.com>\"]"
     platformdirs
     pydantic-settings
     pyparsing
-    tree-sitter
+    tree-sitter-pinned
     tree-sitter-devicetree
   ];
 
