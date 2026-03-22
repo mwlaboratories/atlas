a24d9704c5f2270f92ba0a30f740c8cce36b5bd2
mwlaboratories <tb_vdb@hotmail.com>
2026-03-22 01:18:17 +0100

    dependency cleanup and justfile workflow simplified

---
+++ nix/kiswitch.nix
@@ -0,0 +1,8 @@
+{ fetchzip }:
+
+fetchzip {
+  name = "kiswitch-footprints-2.3";
+  url = "https://github.com/perigoso/keyswitch-kicad-library/releases/download/v2.3/keyswitch-kicad-library.zip";
+  hash = "sha256-RZT/+2VS0DDM8MqJWaU1E5qCTx3wiYnG7KmO0apHGwg=";
+  stripRoot = false;
+}
